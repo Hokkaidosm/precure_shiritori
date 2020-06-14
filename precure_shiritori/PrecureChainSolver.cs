01ed72030e1f64718fd2946649a45cc4d71e3346
Hokkaidosm <64832302+Hokkaidosm@users.noreply.github.com>
2020-06-14 19:40:04 +0900

    最終版

---
+++ precure_shiritori/PrecureChainSolver.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace precure_shiritori
+{
+    class PrecureChainSolver
+    {
+        public List<Precure> PrecureList { get; set; }
+        public PrecureChainSolver(List<Precure> precureList)
+        {
+            PrecureList = precureList.ToList();
+        }
+
+        private Queue<PrecureChainList> _queue = new Queue<PrecureChainList>();
+        public PrecureChainList Solve(Precure precure)
+        {
+            PrecureChainList firstState = new PrecureChainList(PrecureList);
+            firstState.Add(precure);
+            PrecureChainList ans = firstState;
+            _queue.Enqueue(firstState);
+            while (_queue.Count > 0)
+            {
+                var curr = _queue.Dequeue();
+                ans = curr;
+                foreach (var w in Candidate(curr.LastPrecure))
+                {
+                    if (ans.Find(x => x == w) != null)
+                        continue;
+                    curr.Add(w);
+                    _queue.Enqueue(curr.Clone());
+                    curr.RemoveAt(curr.Count - 1);
+                }
+
+            }
+            return ans;
+        }
+        // 候補の単語を列挙する
+        private IEnumerable<Precure> Candidate(Precure precure)
+        {
+            return PrecureList.Where(x => precure.Last == x.First).ToList();
+        }
+    }
+}
--- precure_shiritori/Program.cs
+++ precure_shiritori/Program.cs
@@ -1,15 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using YamlDotNet.Serialization;
 
 namespace precure_shiritori
 {
     class Program
     {
+        /// <summary>
+        /// rubicure/config のパス
+        /// </summary>
+        static readonly string rubicureConfigPath = "../../rubicure/config/";
         static void Main(string[] args)
         {
+            List<Precure> precureList = LoadPrecure();
+            List<PrecureChainList> precureChainLists = new List<PrecureChainList>();
+            DateTime dt = DateTime.Now;
+            foreach (Precure startPrecure in precureList)
+            {
+                PrecureChainSolver pc = new PrecureChainSolver(precureList);
+                precureChainLists.Add(pc.Solve(startPrecure));
+            }
+            var ansList = precureChainLists.OrderByDescending(P => P.Count);
+            int count = 0;
+            foreach (var ans in ansList)
+            {
+                if (ans.Count != count)
+                {
+                    Console.WriteLine("============== {0}人 =================", ans.Count);
+                    count = ans.Count;
+                }
+                ans.GetPrecureList().ToList().ForEach(precure => Console.WriteLine($"{precure.PrecureName}（{precure.PrecureRuby}）"));
+                Console.WriteLine("---------------------------------------------");
+            }
+            Console.WriteLine("所要時間：" + (DateTime.Now - dt));
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 全てのプリキュアのデータを読み込む.
+        /// </summary>
+        /// <returns>プリキュアデータのリスト</returns>
+        static List<Precure> LoadPrecure()
+        {
+            List<Precure> precureList = new List<Precure>();
+            var rootPath = rubicureConfigPath + "girls/";
+            string[] files = Directory.GetFiles(rootPath, "*.yml", SearchOption.TopDirectoryOnly);
+            foreach (string file in files)
+            {
+                var input = new StreamReader(file, Encoding.UTF8);
+                var deserializer = new Deserializer();
+                Dictionary<string, Dictionary<string, object>> girls_tmp = deserializer.Deserialize<Dictionary<string, Dictionary<string, object>>>(input);
+                foreach (var girl in girls_tmp)
+                {
+                    if (girl.Value.Count == 1)
+                    {
+                        // エイリアスは飛ばす
+                        continue;
+                    }
+                    string precureName = null;
+                    if (girl.Value.TryGetValue("precure_name", out object precureNameObj))
+                    {
+                        precureName = (string)precureNameObj;
+                    }
+                    precureList.Add(new Precure(precureName));
+                }
+            }
+
+            return precureList;
         }
     }
 }
