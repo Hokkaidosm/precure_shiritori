61d0a9921926ef47d3f41b6dd15bd5b5d2efcce1
DESKTOP-K4908VJ\nagahama <hokkaido.sm.sm@gmail.com>
2020-06-14 19:40:04 +0900

    最終版

---
+++ precure_shiritori/Precure.cs
@@ -0,0 +1,75 @@
+﻿using System.Text.RegularExpressions;
+
+namespace precure_shiritori
+{
+    /// <summary>
+    /// プリキュアデータ
+    /// </summary>
+    class Precure
+    {
+        /// <summary>
+        /// プリキュア名
+        /// </summary>
+        public string PrecureName { get; private set; }
+        /// <summary>
+        /// 読み
+        /// </summary>
+        public string PrecureRuby { get; private set; }
+        /// <summary>
+        /// 読みの最初の文字
+        /// </summary>
+        public char First { get; private set; }
+        /// <summary>
+        /// 読みの最後の文字
+        /// </summary>
+        public char Last { get; private set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="name">プリキュア名</param>
+        public Precure(string name)
+        {
+            PrecureName = name;
+            PrecureRuby = Regex.Replace(name, "^キュア", "");
+            First = ToSeion(ToChokuon(PrecureRuby[0]));
+            Last = ToSeion(ToChokuon(PrecureRuby[PrecureRuby.Length - 1]));
+            // 長音一文字の単語はない前提
+            if (Last == 'ー' || Last == '－')
+            {
+                Last = ToSeion(ToChokuon(PrecureRuby[PrecureRuby.Length - 2]));
+            }
+        }
+
+        // ッは促音だが、拗音(Youon)という変数名とする
+        private const string Youon = "ァィゥェォヵヶッャュョヮ";
+        private const string Chokuon = "アイウエオカケツヤユヨワ";
+        private char ToChokuon(char c)
+        {
+            int ix = Youon.IndexOf(c);
+            if (ix > 0)
+            {
+                return Chokuon[ix];
+            }
+            return c;
+        }
+
+        //　半濁音を含んでいますが定数名はDakuon（濁音）にします
+        private const string Dakuon = "ガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポ";
+        private const string Seion  = "カキクケコサシスセソタチツテトハヒフヘホハヒフヘホ";
+        private char ToSeion(char c)
+        {
+            int ix = Dakuon.IndexOf(c);
+            if (ix > 0)
+            {
+                return Seion[ix];
+            }
+            return c;
+        }
+
+        public override string ToString()
+        {
+            return this.PrecureName.ToString();
+        }
+    }
+}
---
+++ precure_shiritori/PrecureChainList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace precure_shiritori
+{
+    class PrecureChainList
+    {
+        private List<short> _chain = new List<short>();
+        private List<Precure> AllPrecures { get; set; }
+
+        public PrecureChainList(List<Precure> precureList)
+        {
+            this.AllPrecures = precureList;
+        }
+
+        public Precure LastPrecure
+        {
+            get
+            {
+                int ix = _chain[_chain.Count - 1];
+                return AllPrecures[ix];
+            }
+        }
+
+        public PrecureChainList Clone()
+        {
+            var wcl = new PrecureChainList(this.AllPrecures)
+            {
+                _chain = _chain.ToList()
+            };
+            return wcl;
+        }
+
+        public void Add(Precure precure)
+        {
+            short ix = (short)AllPrecures.FindIndex(x => x.PrecureName == precure.PrecureName);
+            _chain.Add(ix);
+        }
+
+        public Precure Find(Func<Precure, bool> pred)
+        {
+            foreach (var w in GetPrecureList())
+            {
+                if (pred(w) == true)
+                {
+                    return w;
+                }
+            }
+            return null;
+        }
+
+        public IEnumerable<Precure> GetPrecureList()
+        {
+            foreach (var ix in _chain)
+            {
+                yield return AllPrecures[ix];
+            }
+        }
+
+        public int Count
+        {
+            get { return _chain.Count; }
+        }
+
+        internal void RemoveAt(int index)
+        {
+            _chain.RemoveAt(index);
+        }
+    }
+}
